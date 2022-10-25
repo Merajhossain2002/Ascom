2d02a123aa0ccf608e6603dd9197e88ca0da0ec9
Meraj Faizan <mdmaraj1500@gmail.com>
2022-10-25 16:32:24 +0600

    first commit

---
+++ src/__Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBo7iIMWB3RyTznKHtyJ3pAdMlK9UGZsHQ",
+  authDomain: "ascom-fb87b.firebaseapp.com",
+  projectId: "ascom-fb87b",
+  storageBucket: "ascom-fb87b.appspot.com",
+  messagingSenderId: "973798966677",
+  appId: "1:973798966677:web:c39aa0d83c38efeabf229c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
