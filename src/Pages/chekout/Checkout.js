8d1e94564d9e94a7719b70916cf03ec4e8164a86
Meraj Faizan <mdmaraj1500@gmail.com>
2022-10-27 18:03:54 +0600

    toast and checkout page added

--- src/Pages/chekout/Checkout.js
+++ src/Pages/chekout/Checkout.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import Header from '../Shared/Header/Header';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { FaCreditCard, FaCrown } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import Header from "../Shared/Header/Header";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Checkout = () => {
+  const handleCheckout = () => {
+    toast.success(
+      "🦄 Congratulation,Successfully Purchased, your now ASCOM Learner.",
+      {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "colored",
+      }
+    );
+  };
   return (
     <div>
       <Header></Header>
-      <div  className='container'>
-        <p>this is chexk</p>
+      <div className="container text-center">
+        <div className="shadow mt-5 mx-auto p-3" style={{ width: "50vw" }}>
+          <h2 className="text-info">
+            <FaCrown /> Get Premium Access of{" "}
+            <span className="text-warning">Ascom</span>
+          </h2>
+          <h4 className="mt-4">$99 /Per Month</h4>
+          <Button
+            onClick={handleCheckout}
+            className=" border-warning border-2 shadow"
+            variant="light"
+          >
+            <FaCreditCard className="me-2" />
+            Purchase
+          </Button>
+        </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
